633c409b1481a1d01e4630df198cb6e9d6b2f0bd
asakura <asakuradev@gmail.com>
2015-10-28 01:03:07 +0900

    add top page(login page)

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users, path_names: { sign_in: "login", sign_out: "logout"},
              controllers: { omniauth_callbacks: "omniauth_callbacks"}
-  get 'home/index'
-
-  get 'home/show'
-
-  root to: "home#show"
+  root to: "home#index"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
